341723dc082b187e2aa5ae5a68f8dab945c22bee
339498784 <339498784@2260I-L29-009>
2023-03-07 11:05:04 -0500

    in porgress

---
+++ Part 5.5 More Classes/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5._5_More_Classes
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+        private int _sides;
+
+        public Die(int sides, int roll)
+        {
+            _sides = sides;
+            if (roll < 1 || roll > sides)
+            {
+                throw new ArgumentException("Roll must be within die range", "roll");
+            }
+            _roll = roll;
+            _generator = new Random();
+        }
+
+        public int Roll { get { return _roll; } }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, _sides + 1);
+            return _roll;
+        }
+
+        public void DrawFace()
+        {
+            string path = (@"DieFace.txt");
+        }
+    }
+}
--- Part 5.5 More Classes/Program.cs
+++ Part 5.5 More Classes/Program.cs
@@ -10,6 +10,15 @@ namespace Part_5._5_More_Classes
     {
         static void Main(string[] args)
         {
+            Die dice6Sides = new Die(6, 6);
+
+            Console.WriteLine(dice6Sides.Roll);
+
+            for(int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(dice6Sides.RollDie());
+            }
+            Console.ReadLine();
         }
     }
 }
