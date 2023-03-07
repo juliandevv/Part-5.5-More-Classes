4ce9d14d4a4e380b89ca9dd53236cfcf196aaebf
339498784 <339498784@2260I-L29-009>
2023-03-07 10:28:45 -0500

    Add project files.

---
+++ Part 5.5 More Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5._5_More_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
