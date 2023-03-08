d04a28db1ea81758bc88a0d5bda4dc93ca1b14ce
339498784 <339498784@2260I-L29-009>
2023-03-08 11:02:57 -0500

    finished loaded dice started game

--- Part 5.5 More Classes/Dice.cs
+++ Part 5.5 More Classes/Dice.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Part_5._5_More_Classes
 {
@@ -11,6 +12,7 @@ namespace Part_5._5_More_Classes
         private Random _generator;
         private int _roll;
         private int _sides;
+        private int[] _loadedNums;
 
         public Die(int sides, int roll)
         {
@@ -23,6 +25,18 @@ namespace Part_5._5_More_Classes
             _generator = new Random();
         }
 
+        public Die(int sides, int roll, int[] loadedNums)
+        {
+            _sides = sides;
+            if (roll < 1 || roll > sides)
+            {
+                throw new ArgumentException("Roll must be within die range", "roll");
+            }
+            _roll = roll;
+            _generator = new Random();
+            _loadedNums = loadedNums;
+        }
+
         public int Roll { get { return _roll; } }
 
         public int RollDie()
@@ -31,9 +45,39 @@ namespace Part_5._5_More_Classes
             return _roll;
         }
 
+        public int RollWeightedDie()
+        {
+            double factor = _generator.NextDouble();
+            _roll = _generator.Next(1, _sides + 1);
+            //Console.WriteLine(_roll);
+            foreach (int loadedNum in _loadedNums)
+            {
+                if (loadedNum == _roll - 1)
+                {
+                    //Console.WriteLine(Convert.ToDouble(_roll) - factor);
+                    _roll = Convert.ToInt32(Math.Round(Convert.ToDouble(_roll) - factor));
+                }
+                else if (loadedNum == _roll + 1)
+                {
+                    //Console.WriteLine(Convert.ToDouble(_roll) + factor);
+                    _roll = Convert.ToInt32(Math.Round(Convert.ToDouble(_roll) + factor));
+                }
+            }
+            //Console.WriteLine(_roll);
+            return _roll;
+        }
+
         public void DrawFace()
         {
-            string path = (@"DieFace.txt");
+            string path = (@"Die Faces.txt");
+            string[] lines = File.ReadAllLines(path);
+
+            lines[3] = $" \\   {_roll}   /";
+
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
--- Part 5.5 More Classes/Program.cs
+++ Part 5.5 More Classes/Program.cs
@@ -10,15 +10,37 @@ namespace Part_5._5_More_Classes
     {
         static void Main(string[] args)
         {
-            Die dice6Sides = new Die(6, 6);
+            //frequency();
 
-            Console.WriteLine(dice6Sides.Roll);
+            string bar = new string('=', Console.WindowWidth);
+            string title = " Welcome to DICE MANIA! ";
+            Console.WriteLine(bar);
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (title.Length / 2), 0);
+            Console.WriteLine(title);
 
-            for(int i = 0; i < 20; i++)
-            {
-                Console.WriteLine(dice6Sides.RollDie());
-            }
             Console.ReadLine();
+
+            void frequency()
+            {
+                int[] loads = new int[] { 2, 4 }; //loaded numbers should not be consecutive
+                Die dice6Sides = new Die(6, 1, loads);
+                int[] rolls = new int[1000];
+                int roll = 0;
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    roll = dice6Sides.RollWeightedDie();
+                    rolls[i] = roll;
+                }
+
+                foreach (int unique in rolls.Distinct<int>())
+                {
+                    Console.WriteLine("Roll: {0} Frequency: {1}", unique, rolls.Count(x => x == unique));
+                }
+
+                Console.ReadLine();
+            }
+            
         }
     }
 }
