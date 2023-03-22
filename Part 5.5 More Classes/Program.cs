087e6a03304681d148c5c3b726211bc7cefbcfcf
339498784 <339498784@2260I-L29-009>
2023-03-22 10:59:47 -0400

    move animation method to dice class

--- Part 5.5 More Classes/Program.cs
+++ Part 5.5 More Classes/Program.cs
@@ -7,12 +7,13 @@ using System.IO;
 using System.Media;
 using System.Runtime.Remoting.Messaging;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace Part_5._5_More_Classes
 {
     internal class Program
     {
-        enum BetMode
+        enum Bet
         {
             doubles,
             notDoubles,
@@ -21,155 +22,185 @@ namespace Part_5._5_More_Classes
         }
         static void Main(string[] args)
         {
-            BetMode betMode = BetMode.doubles;
+            //Write title
             string bar = new string('=', Console.WindowWidth);
             string title = " WELCOME TO DICE MANIA! ";
             Console.WriteLine(bar);
             Console.SetCursorPosition((Console.WindowWidth / 2) - (title.Length / 2), Console.CursorTop - 2);
-            Console.WriteLine(title);
+            animateString(title);
 
+            //main command prompt loop
             bool exit = false;
             while (exit == false)
             {
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write(">>>");
-                Parse(Console.ReadLine());
-            }
-            
-            void Parse(string commandString)
-            {
-                List<string> commandParts = commandString.Split(' ').ToList();
-                string command = commandParts.First();
-                List<string> arguments = commandParts.Skip(1).ToList();
-
-                switch (command){
-
-                    case "help":
-                        string[] lines = File.ReadAllLines(@"help.txt");
-                        foreach(string line in lines)
-                        {
-                            Console.WriteLine(line);
-                        }
-                        break;
-                    case "play":
-                        play();
-                        break;
-                    case "exit":
-                        exit = true;
-                        break;
-                    default:
-                        Console.WriteLine("Not a valid command. Use 'help' for list of valid commands");
-                        break;
+                if(Parse(Console.ReadLine()) == true)
+                {
+                    exit = true;
                 }
             }
+        }
+        static bool Parse(string commandString)
+        {
+            //parse input to commands
+            List<string> commandParts = commandString.Split(' ').ToList();
+            string command = commandParts.First();
+            List<string> arguments = commandParts.Skip(1).ToList();
+            Die die = new Die(6, 1);
 
-            void play()
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                int balance = 100;
-                int bet = 0;
-                Console.WriteLine($"You currently have: {balance.ToString("C")}");
-                Console.WriteLine("Choose a betting option fom the following:");
-                string[] lines = File.ReadAllLines(@"bets.txt");
-                foreach(string line in lines)
-                {
-                    Console.WriteLine(line);
-                }
-
-                ConsoleKey option = Console.ReadKey().Key;
-                switch (option)
-                {
-                    case ConsoleKey.A:
-                        Console.WriteLine("\nYou are betting on doubles. If you win, your bet will be doubled.");
-                        betMode = BetMode.doubles;
-                        break;
-                    case ConsoleKey.C:
-                        Console.WriteLine("\nYou are betting on odd sum. If you win, you will get your bet.");
-                        betMode = BetMode.notDoubles;
-                        break;
-                    case ConsoleKey.B:
-                        Console.WriteLine("\nYou are betting on not doubles. If you win, you will get half your bet.");
-                        betMode = BetMode.oddSum;
-                        break;
-                    case ConsoleKey.D:
-                        Console.WriteLine("\nYou are betting on even sum. If you win, you will get your bet.");
-                        betMode = BetMode.evenSum;
-                        break;
-                    default:
-                        Console.WriteLine("\nNot a valid selection1");
-                        break;
-                }
-
-                Console.WriteLine("Enter the mount you would like to bet:");
-                bet = ParseResponse(balance);
-                Console.WriteLine($"You are betting: {bet.ToString("C")}");
-                animateDice();
-                //Console.SetCursorPosition(Console.CursorLeft + 13, Console.CursorTop - 7);
-                //drawDice(0);
-            }
-
-            int ParseResponse(int balance)
-            {
-                while (true)
-                {
-                    string response = Console.ReadLine();
-                    if (int.TryParse(response, out int num))
+            //evaluate command
+            switch (command){
+                //display help screen
+                case "help":
+                    string[] lines = File.ReadAllLines(@"help.txt");
+                    foreach(string line in lines)
                     {
-                        if (num < 0)
-                        {
-                            num = 0;
-                        }
-                        else if (num > balance)
-                        {
-                            num = balance;
-                        }
-                        return num;
+                        Console.WriteLine(line);
                     }
-                    else
+                    break;
+                //main game 
+                case "play":
+                    play(die);
+                    break;
+                //exit condition
+                case "exit":
+                    return true;
+                default:
+                    Console.WriteLine("Not a valid command. Use 'help' for list of valid commands");
+                    break;
+            }
+            return false;
+        }
+
+        static void play(Die die)
+        {
+            //initialize gameplay vars
+            Random genertaor = new Random();
+            Bet betMode = Bet.doubles;
+            Bet betResult = Bet.doubles;
+            Console.ForegroundColor = ConsoleColor.Green;
+            int balance = 100;
+            int bet = 0;
+            int roll1 = 0, roll2 = 0;
+
+            //prompt betting choices
+            animateString($"You currently have: {balance.ToString("C")}");
+            animateString("Choose a betting option fom the following:");
+            Console.WriteLine();
+            string[] lines = File.ReadAllLines(@"bets.txt");
+
+            foreach(string line in lines)
+            {
+                animateString(line);
+            }
+
+            ConsoleKey option = Console.ReadKey().Key;
+            Console.WriteLine();
+            switch (option)
+            {
+                case ConsoleKey.A:
+                    animateString("\nYou are betting on doubles. If you win, your bet will be doubled.");
+                    betMode = Bet.doubles;
+                    break;
+                case ConsoleKey.C:
+                    animateString("\nYou are betting on odd sum. If you win, you will get your bet.");
+                    betMode = Bet.notDoubles;
+                    break;
+                case ConsoleKey.B:
+                    animateString("\nYou are betting on not doubles. If you win, you will get half your bet.");
+                    betMode = Bet.oddSum;
+                    break;
+                case ConsoleKey.D:
+                    animateString("\nYou are betting on even sum. If you win, you will get your bet.");
+                    betMode = Bet.evenSum;
+                    break;
+                default:
+                    animateString("\nNot a valid selection1");
+                    break;
+            }
+
+            animateString("Enter the mount you would like to bet:");
+            bet = ParseResponse(balance);
+            animateString($"\nYou are betting: {bet.ToString("C")}");
+            animateString("Press SPACE to roll dice!");
+
+            //animate dice when space key pressed
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+                {
+                    animateDice(die, genertaor, roll1, roll2);
+                    break;
+                }
+            }
+
+            //final roll
+            roll1 = die.RollDie();
+            roll2 = die.RollDie();
+            if (roll1 == roll2)
+            {
+                betResult = Bet.doubles;
+            }
+        }
+
+        static int ParseResponse(int balance)
+        {
+            while (true)
+            {
+                string response = Console.ReadLine();
+                if (int.TryParse(response, out int num))
+                {
+                    if (num < 0)
                     {
-                        Console.WriteLine("Not a valid Input!");
+                        num = 0;
                     }
+                    else if (num > balance)
+                    {
+                        num = balance;
+                    }
+                    return num;
                 }
-            }
-
-            void animateDice()
-            {
-                int[]
-                drawDie(0);
-            }
-
-            void drawDie(int[] rolls)
-            {
-                Console.Write("\n ___________    ___________");
-                Console.Write("\n|~---------~|  |~---------~|");
-                Console.Write("\n|           |  |           |");
-                Console.Write($"\n|     {rolls[0]}     |  |     {rolls[1]}     |");
-                Console.Write("\n|           |  |           |");
-                Console.Write("\n|___________|  |___________|\n");
-
-            }
-
-            void frequency()
-            {
-                int[] loads = new int[] { 2, 4 }; //loaded numbers should not be consecutive
-                Die dice6Sides = new Die(6, 1, loads);
-                int[] rolls = new int[1000];
-                int roll = 0;
-
-                for (int i = 0; i < 1000; i++)
+                else
                 {
-                    roll = dice6Sides.RollWeightedDie();
-                    rolls[i] = roll;
+                    Console.WriteLine("Not a valid Input!");
                 }
-
-                foreach (int unique in rolls.Distinct<int>())
-                {
-                    Console.WriteLine("Roll: {0} Frequency: {1}", unique, rolls.Count(x => x == unique));
-                }
-
-                Console.ReadLine();
             }
-            
+        }
+
+        static void animateDice(Die die, Random generator, int roll1, int roll2)
+        {
+            List<ConsoleColor> colors = new List<ConsoleColor>() { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Cyan, ConsoleColor.Magenta};
+            for (int i = 0; i < 20; i++)
+            {
+                roll1 = die.RollDie();
+                roll2 = die.RollDie();
+                Console.ForegroundColor = colors[generator.Next(0, 5)];
+                drawDice(roll1, roll2);
+                Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 7);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void drawDice(int roll1, int roll2)
+        {
+            Console.Write("\n ___________    ___________");
+            Console.Write("\n|~---------~|  |~---------~|");
+            Console.Write("\n|           |  |           |");
+            Console.Write($"\n|     {roll1}     |  |     {roll2}     |");
+            Console.Write("\n|           |  |           |");
+            Console.Write("\n|___________|  |___________|\n");
+
+        }
+
+        static void animateString(string text)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c.ToString());
+                Thread.Sleep(10);
+            }
+            Console.WriteLine();
         }
     }
 }
